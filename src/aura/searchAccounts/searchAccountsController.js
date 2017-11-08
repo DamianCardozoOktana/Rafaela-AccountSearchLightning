64b8823770fe28b9bfde8a55f44b98be69ca315d
Rafaela Vasconcellos <rafaela@oktana.io>
2017-11-08 13:54:38 -0300

    Search Accounts by name

--- src/aura/searchAccounts/searchAccountsController.js
+++ src/aura/searchAccounts/searchAccountsController.js
@@ -1,8 +1,18 @@
 ({
-	searchAccounts : function(component, event, helper) {
+	
+    searchAccounts: function(component, event) {
         if(event.getParams().keyCode == 13){
-          	var accountName = component.find('name').get('v.value');
-        	alert(accountName);
+          	var action = component.get("c.getAccountsByName");
+            action.setParams({ "name": component.get('v.accountName') });
+            action.setCallback(this, function(response) {
+                component.set("v.accounts", response.getReturnValue());
+            });
+            
+            $A.enqueueAction(action);
+            
+            var toggleTable = component.find("accounts--table");
+	        $A.util.toggleClass(toggleTable, "toggle--table");
         }
     }
+
 })
